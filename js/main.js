f0538433e247487449ce5d5b292c29b78b4e14cb
GulyaevSergey <109235648+GulyaevSergey@users.noreply.github.com>
2022-08-21 23:30:52 +0700

    movement by cards commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,34 @@
-const test = 1;
+// Движение вперед
+const btnNext = document.querySelectorAll('[data-nav="next"]');
+btnNext.forEach(function(button){
+    button.addEventListener("click", function(){
+        const thisCard = this.closest("[data-card]");
+
+        navigate("next", thisCard);
+    })
+})
+
+// Движение назад
+const btnPrev = document.querySelectorAll('[data-nav="prev"]');
+btnPrev.forEach(function(button){
+    button.addEventListener("click", function(){
+        const thisCard = this.closest("[data-card]");
+
+        navigate("prev", thisCard);
+
+    })
+})
+
+function navigate(direction, thisCard) {
+    const thisCardNumber = parseInt(thisCard.dataset.card);
+    var nextCard;
+
+    if(direction == "next") {
+        nextCard = thisCardNumber + 1;
+    } else if (direction == "prev") {
+        nextCard = thisCardNumber - 1;
+    }
+
+    thisCard.classList.add("hidden");
+    document.querySelector(`[data-card="${nextCard}"]`).classList.remove("hidden");
+}
